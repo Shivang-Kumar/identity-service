5c2b077677823d6abd6c59842c904eaeb1a23c71
Shivang Kumar <2002shiva.2002@gmail.com>
2026-01-22 22:20:51 +0530

    Created identity-service

---
+++ src/test/java/com/practice/microservices/identity_service/IdentityServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.practice.microservices.identity_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IdentityServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
