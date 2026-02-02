165a1faa616ad1c80b73a80fc0928e12b4a9af03
Shivang Kumar <2002shiva.2002@gmail.com>
2026-02-02 23:24:45 +0530

    Added unit test for register User controller

---
+++ src/test/java/com/practice/microservices/identity_service/user/UserController.java
@@ -0,0 +1,61 @@
+package com.practice.microservices.identity_service.user;
+
+import static org.mockito.BDDMockito.given;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+
+import java.time.Instant;
+import java.util.UUID;
+
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.practice.microservices.identity_service.dtos.UserDto;
+import com.practice.microservices.identity_service.entity.AccountStatus;
+import com.practice.microservices.identity_service.entity.Role;
+import com.practice.microservices.identity_service.entity.UserEntity;
+import com.practice.microservices.identity_service.services.UserService;
+import com.practice.microservices.identity_service.utility.Result;
+
+
+@SpringBootTest
+@AutoConfigureMockMvc(addFilters=false)
+public class UserController {
+	
+	@Autowired
+	MockMvc mockMvc;
+	
+	@MockBean
+	UserService userService;
+	
+	ObjectMapper objectMapper=new ObjectMapper();
+	
+	
+	
+	@Test
+	void registerUserSuccess() throws Exception
+	{
+		UserDto userDto=new UserDto("abc@gmail.com","myPassword","shiv","kumar",78405014,Role.CUSTOMER);
+		String json=this.objectMapper.writeValueAsString(userDto);
+		UserEntity user=new UserEntity(null, "abc@gmail.com","myPassword","shiv","kumar",78405014,Role.CUSTOMER,AccountStatus.DISABLED,false, Instant.now(), Instant.now(),Instant.now());
+		
+		given(this.userService.registerUser(Mockito.any(UserDto.class))).willReturn(new Result(true,"Account Created Successfully",user));
+
+		
+		this.mockMvc.perform(post("/api/v1/auth/register").contentType(MediaType.APPLICATION_JSON).content(json)
+		.accept(MediaType.APPLICATION_JSON))
+		.andExpect(jsonPath("$.flag").value("true"))
+		.andExpect(jsonPath("$.message").value("Account Created Successfully"));
+
+	}
+	
+	
+
+}
