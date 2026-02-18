f4fa55e51675afe33ad33e9faaf4afcbd1810fe4
Shivang Kumar <2002shiva.2002@gmail.com>
2026-02-18 23:24:33 +0530

    Fixed bugs related to jwt

--- src/main/java/com/practice/microservices/identity_service/services/RedisCacheClient.java
+++ src/main/java/com/practice/microservices/identity_service/services/RedisCacheClient.java
@@ -2,18 +2,19 @@ package com.practice.microservices.identity_service.services;
 
 import java.util.concurrent.TimeUnit;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Service;
 
-import lombok.RequiredArgsConstructor;
+
 
 @Service
-@RequiredArgsConstructor
 public class RedisCacheClient {
 	
+	@Autowired
 	private StringRedisTemplate redisTemplate;
 	
-	public void set(String key,String value,int timeout,TimeUnit unit)
+	public void set(String key,String value,Long timeout,TimeUnit unit)
 	{
 		redisTemplate.opsForValue().set(key, value,timeout,unit);
 	}
--- src/main/java/com/practice/microservices/identity_service/services/UserServiceImpl.java
+++ src/main/java/com/practice/microservices/identity_service/services/UserServiceImpl.java
@@ -23,31 +23,27 @@ import lombok.AllArgsConstructor;
 @Service
 @AllArgsConstructor
 public class UserServiceImpl implements UserService {
-		
+
 	UserRepository userRepository;
 	UserDtoToUserConverter userConverter;
 	PasswordEncoder passwordEncoder;
 	RedisCacheClient redisTemplate;
 	JwtUtils jwtUtils;
- 
+
 	@Override
 	public Result registerUser(UserDto registerDto) {
-	
+
 		try {
-		userRepository.findByEmail(registerDto.email()).ifPresent(user -> {
-			throw new RuntimeException("User with this email already exits");
-		});
-		UserEntity user=userConverter.convertRegisterDtoToUser(registerDto);
-		user.setPassword(passwordEncoder.encode(registerDto.password()));
-		UserEntity savedUser=userRepository.save(user);
-		return new Result(true,"Account Created Successfully",savedUser);
-		}
-		catch(RuntimeException e)
-		{
+			userRepository.findByEmail(registerDto.email()).ifPresent(user -> {
+				throw new RuntimeException("User with this email already exits");
+			});
+			UserEntity user = userConverter.convertRegisterDtoToUser(registerDto);
+			user.setPassword(passwordEncoder.encode(registerDto.password()));
+			UserEntity savedUser = userRepository.save(user);
+			return new Result(true, "Account Created Successfully", savedUser);
+		} catch (RuntimeException e) {
 			throw e;
-		}
-		catch(Exception e)
-		{
+		} catch (Exception e) {
 			System.out.println(e);
 			throw new RuntimeException("Could not save user to database, please try again after sometime!");
 		}
@@ -55,46 +51,41 @@ public class UserServiceImpl implements UserService {
 
 	@Override
 	public Result getAllUsers() {
-		List<UserEntity> allUsers=userRepository.findAll();
-		return new Result(true,"All Users Registered",allUsers);
+		List<UserEntity> allUsers = userRepository.findAll();
+		return new Result(true, "All Users Registered", allUsers);
 	}
 
 	@Override
 	public Result getUserById(UUID id) {
-		Optional<UserEntity> user=userRepository.findById(id);
-		return  new Result(true,"Found User By Id",user.get());
+		Optional<UserEntity> user = userRepository.findById(id);
+		return new Result(true, "Found User By Id", user.get());
 	}
 
 	@Override
-	public Result updateUserById(UUID userId,UserDto updatedUser) {
-		
-		UserEntity savedUser= userRepository.findById(userId).map(oldUser -> {
+	public Result updateUserById(UUID userId, UserDto updatedUser) {
+
+		UserEntity savedUser = userRepository.findById(userId).map(oldUser -> {
 			oldUser.setUpdatedAt(Instant.now());
 			oldUser.setPhone(updatedUser.phone());
 			oldUser.setFirstName(updatedUser.firstName());
 			oldUser.setLastName(updatedUser.lastName());
-			
+
 			return this.userRepository.save(oldUser);
 		}).orElseThrow(() -> new RuntimeException("Cannot find user with this UUID"));
-		
-		return new Result(true,"User Updated Successfully",savedUser);
+
+		return new Result(true, "User Updated Successfully", savedUser);
 	}
-//
-//	@Override
-//	public void invalidateToken(HttpServletRequest request) {
-//		String authHeader=request.getHeader("Authorization");
-//		if(authHeader==null||authHeader.startsWith("Bearer ")==false)
-//		{
-//			throw new RuntimeException("Bad Request JWT Token is required");
-//		}
-//		
-//		String token=authHeader.substring(7);
-//		String timeLeft=this.jwtUtils.getTimeLeft(token);
-//		redisTemplate.set("Blacklisted:"+token, "true" , timeLeft, TimeUnit.MILLISECONDS);
-//		
-//		
-//	}
-	
-	
-	
+
+	@Override
+	public void invalidateToken(HttpServletRequest request) {
+		String authHeader = request.getHeader("Authorization");
+		if (authHeader == null || authHeader.startsWith("Bearer ") == false) {
+			throw new RuntimeException("Bad Request JWT Token is required");
+		}
+
+		String token = authHeader.substring(7);
+		Long timeLeft = this.jwtUtils.getTimeLeft(token);
+		redisTemplate.set("Blacklisted:" + token, "true", timeLeft, TimeUnit.MILLISECONDS);
+	}
+
 }
